6f809f53d056c6f87cfb2d065cb91c4c2a71ea4b
Ron Balthazor <ron.balthazor@gmail.com>
2012-02-02 16:37:27 -0500

    Mod. to Courses public

--- src/English/CoursesBundle/Controller/DefaultController.php
+++ src/English/CoursesBundle/Controller/DefaultController.php
@@ -6,14 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * Courses Public controller.
+ *
+ * @Route("/listings")
+ */
+
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * Lists all Courses.
+     *
+     * @Route("/", name="listings")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        $currentTerm = '201202';
+        $em = $this->get('doctrine.orm.entity_manager');
+        $dql1 = "SELECT c.courseName,c.title,c.instructorName,c.callNumber,c.callNumber2,c.days,c.time,c.id FROM English\CoursesBundle\Entity\Course c WHERE c.term = ?1 ORDER BY c.courseName";
+        $courses = $em->createQuery($dql1)->setParameter('1','201202')->getResult();
+        $dql2 = "SELECT t.termName,t.term FROM English\TermBundle\Entity\Term t WHERE t.type >= ?1 ORDER BY t.term";
+        $terms = $em->createQuery($dql2)->setParameter('1','1')->getResult();
+        return $this->render('EnglishCoursesBundle:Default:index.html.twig', array('courses' => $courses,'terms' => $terms,'currentTerm' => $currentTerm)); 
     }
 }
