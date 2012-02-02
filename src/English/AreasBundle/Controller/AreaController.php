86e51d9a68d5c379ad1725220f514d22307390c2
Ron Balthazor <ron.balthazor@gmail.com>
2012-02-02 15:26:24 -0500

    Mod. to Area Controller

--- src/English/AreasBundle/Controller/AreaController.php
+++ src/English/AreasBundle/Controller/AreaController.php
@@ -154,7 +154,7 @@ class AreaController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('area_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('area_show', array('id' => $id)));
         }
 
         return array(
