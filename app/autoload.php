fcf26159057cb1556f60e5e1c0dfda2b1e86c76a
rlbaltha <rlbaltha@ubuntu.ubuntu-domain>
2012-02-28 18:01:14 -0500

    Email added for Users and Mentoring

--- app/autoload.php
+++ app/autoload.php
@@ -15,6 +15,7 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'FOS'              => __DIR__.'/../vendor/bundles',
+    'FR3D'             => __DIR__.'/../vendor/bundles',
     'Stof'             => __DIR__.'/../vendor/bundles',
     'Gedmo'            => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
 ));
