729632410dd3a934827dcccf10d14deb45e71c24
Ron Balthazor <ron.balthazor@gmail.com>
2013-10-16 10:46:41 -0400

    fosuser override removed

--- src/English/AuthenticateBundle/Controller/RegistrationController.php
+++ src/English/AuthenticateBundle/Controller/RegistrationController.php
@@ -18,36 +18,5 @@ use English\GradinfoBundle\Entity\Gradinfo;
 
 class RegistrationController extends BaseController
 {
-    public function registerAction()
-    {
-        $form = $this->container->get('fos_user.registration.form');
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
-
-        $process = $formHandler->process($confirmationEnabled);
-        if ($process) {
-            $user = $form->getData();
-            
-
-
-            if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
-                $route = 'fos_user_registration_check_email';
-            } else {
-                $this->authenticateUser($user);
-                $route = 'fos_user_registration_confirmed';
-            }
-
-            $this->setFlash('fos_user_success', 'registration.flash.user_created');
-            $url = $this->container->get('router')->generate($route);
-
-            return new RedirectResponse($url);
-        }
-
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
-            'form' => $form->createView(),
-            'theme' => $this->container->getParameter('fos_user.template.theme'),
-        ));
-    }
 
 }
