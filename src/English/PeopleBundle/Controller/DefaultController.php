e1904e8294a7c12e76fff140c9cf684474a944e4
Ron Balthazor <ron.balthazor@gmail.com>
2014-08-30 01:04:18 -0400

    tweaks to homepage display4

--- src/English/PeopleBundle/Controller/DefaultController.php
+++ src/English/PeopleBundle/Controller/DefaultController.php
@@ -62,7 +62,7 @@ class DefaultController extends Controller
             'action' => $this->generateUrl('directory'),
             'method' => 'POST',
         ));
-        $form->add('lastName', 'text', array('label' => 'Search', 'attr' => array('class' => 'form-control', 'placeholder' => 'Lastname'),));
+        $form->add('lastName', 'text', array('label' => ' ', 'attr' => array('size'=>'10','class' => 'form-control', 'placeholder' => 'Lastname'),));
 
         return $form;
     }
